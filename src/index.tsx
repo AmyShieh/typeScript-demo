f489d90a38ad0dc9502af081482ef31ad649c98d
xiechangqing <576535019@qq.com>
2018-09-27 07:52:43 +0800

    init project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import * as React from "react";
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-import { Hello } from './components/Hello';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("root")
+  <App />,
+  document.getElementById('root') as HTMLElement
 );
+registerServiceWorker();
